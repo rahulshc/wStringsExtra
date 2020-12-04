982f09cd9c789a211b26a024d6a5c7ec04617aac
Dmytro <dm.vict.kr@gmail.com>
2020-12-04 10:06:07 +0200

    Extend routine `strRequestParse` by option `subjectWinPathsMaybe`, improve parsing of commands with Windows path

--- proto/wtools/abase/l5/StringTools.s
+++ proto/wtools/abase/l5/StringTools.s
@@ -2739,6 +2739,9 @@ function strRequestParse( o )
     //
     // }
 
+    if( o.subjectWinPathsMaybe )
+    subject = winPathSubjectCheck( subject, map );
+
     result.subjects.push( subject );
     result.maps.push( map );
   }
@@ -2773,6 +2776,23 @@ function strRequestParse( o )
     });
     return result.map( ( e ) => _.numberFromStrMaybe( e ) );
   }
+
+  /* */
+
+  function winPathSubjectCheck( subject, srcMap )
+  {
+
+    for( let key in srcMap )
+    {
+      if( _.strIs( srcMap[ key ] ) && _.strBegins( srcMap[ key ], '\\' ) && key.length === 1 )
+      {
+        subject += ` ${ key }:${ srcMap[ key ] }`;
+        delete srcMap[ key ];
+      }
+    }
+
+    return subject;
+  }
 }
 
 var defaults = strRequestParse.defaults = Object.create( null );
@@ -2782,6 +2802,7 @@ defaults.quoting = 1;
 defaults.unquoting = 1;
 defaults.parsingArrays = 1;
 defaults.severalValues = 0;
+defaults.subjectWinPathsMaybe = 0;
 defaults.src = null;
 
 //
